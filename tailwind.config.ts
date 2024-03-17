36a630dd8dd32b9ca144ca9427cb76d4bdb25c54
NapolDeveloper <loma1016@gmail.com>
2024-03-17 16:56:11 +0900

    [Style] 술닥 기본 색상 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'suldak-gray-900': '#1e1e1e',
+        'suldak-gray-700': '#646464',
+        'suldak-gray-600': '#8e8e8e',
+        'suldak-gray-500': '#bebebe',
+        'suldak-orange-500': '#ff9300',
+        'suldak-green-500': '#20bf27',
+        'suldak-red-500': '#e45141',
+        'suldak-mint-500': '#08beca',
+        'suldak-mint-50': '#f6fdfe',
       },
     },
   },
