63194c9cb33eff98d1757d2d0445f04b28b545a5
thisisthewa2 <thisisthewa2@gmail.com>
2024-06-18 16:23:41 +0900

    [Feat]: SearchIcon에 Link 연결

--- components/liquor/search/SearchInput.tsx
+++ components/liquor/search/SearchInput.tsx
@@ -1,10 +1,15 @@
 'use client';
+import { useState } from 'react';
 import SearchIcon from 'assets/icons/ico-search-gray.svg';
 import ClearIcon from 'assets/icons/ico-clear-gray.svg';
-import { useState } from 'react';
+import Link from 'next/link';
 
 function SearchInput() {
   const [searchValue, setSearchValue] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
 
   const handleClear = () => {
     setSearchValue('');
@@ -12,12 +17,14 @@ function SearchInput() {
 
   return (
     <div className="relative w-full pt-1">
-      <SearchIcon className="absolute left-4 top-1/2 transform -translate-y-1/2" />
+      <Link href="/liquor/search/result">
+        <SearchIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 cursor-pointer" />
+      </Link>
       <input
         className="search-input pl-10 bg-suldak-gray-200 w-full py-2 border-none outline-none focus:ring-0 rounded-full"
         type="text"
         value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={handleInputChange}
         placeholder="원하는 술을 바로 검색해보세요!"
       />
       {searchValue && (
