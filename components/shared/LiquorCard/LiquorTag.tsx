07e5ab9f6a9b963a6012753955cc789b50605d91
thisisthewa2 <thisisthewa2@gmail.com>
2024-06-19 22:07:16 +0900

    [Feat]: Liquor Tag 수정

--- components/shared/LiquorCard/LiquorTag.tsx
+++ components/shared/LiquorCard/LiquorTag.tsx
@@ -1,8 +1,8 @@
 import { LiquorTagProps } from './types';
-function LiquorTag({ tag }: LiquorTagProps) {
+function LiquorTag({ name }: LiquorTagProps) {
   return (
     <div className="flex items-center justify-center bg-white py-1 px-1.5 rounded-sm border border-suldak-gray-300 text-suldak-gray-600 text-xs font-medium">
-      {tag}
+      {name}
     </div>
   );
 }
