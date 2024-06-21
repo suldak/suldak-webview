a4d5ab7d505b2c673ea6f05a3e9839381ac0d4e3
thisisthewa2 <thisisthewa2@gmail.com>
2024-06-21 15:12:36 +0900

    [Feat] 검색 정렬 api 연결

--- apis/liquor/useLiquorSearch.ts
+++ apis/liquor/useLiquorSearch.ts
@@ -5,25 +5,27 @@ import { Liquor } from 'models/liquor';
 
 const getLiquorSearch = async (
   tag: string,
+  sort: boolean,
 ): Promise<ResponseType<{ content: Liquor[] }>> => {
   const { data } = await axiosInstance.get<ResponseType<{ content: Liquor[] }>>(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/liquor/view/liquor-search/`,
     {
       params: {
         searchTag: tag,
+        isRecommend: sort,
       },
     },
   );
   return data;
 };
 
-export const useLiquorSearch = (tag: string, options?: unknown) => {
+export const useLiquorSearch = (tag: string, option?: string) => {
   const queryKey = tag ? ['liquor-search', tag] : [];
 
+  const sort = option === '인기순' ? false : true;
   const { data } = useQuery({
     queryKey,
-    queryFn: () => getLiquorSearch(tag),
-    ...options,
+    queryFn: () => getLiquorSearch(tag, sort),
   });
 
   return {
--- components/liquor/search/SortDropDown.tsx
+++ components/liquor/search/SortDropDown.tsx
@@ -1,20 +1,28 @@
 'use client';
 import React, { useState } from 'react';
 import SortIcon from 'assets/icons/ico-filter-sort.svg';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 type SortOption = '정확도순' | '인기순';
 
 function SortDropDown() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isSelected, setIsSelected] = useState<SortOption>('정확도순');
+  const [isOpen, setIsOpen] = useState(false); // 드롭다운 메뉴를 열고닫기 위함
+  const [isSelected, setIsSelected] = useState<SortOption>('정확도순'); // 메뉴 선택, 텍스트 적용을 위함
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  //const currentSort = (searchParams.get('sort') as SortOption) || '정확도순';
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
   const handleOptionClick = (option: SortOption) => {
-    setIsSelected(option);
     setIsOpen(false);
+    setIsSelected(option);
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('sort', option);
+    router.push(`?${params.toString()}`);
   };
 
   return (
@@ -31,8 +39,8 @@ function SortDropDown() {
           <div
             className={`mt-3 px-4 ${
               isSelected === '정확도순'
-                ? ''
-                : 'font-semibold text-suldak-gray-600'
+                ? 'text-suldak-gray-900'
+                : 'text-suldak-gray-600'
             }`}
             onClick={() => handleOptionClick('정확도순')}
           >
@@ -40,7 +48,9 @@ function SortDropDown() {
           </div>
           <div
             className={`mt-3 px-4 ${
-              isSelected === '인기순' ? '' : 'text-suldak-gray-600'
+              isSelected === '인기순'
+                ? 'text-suldak-gray-900'
+                : 'text-suldak-gray-600'
             }`}
             onClick={() => handleOptionClick('인기순')}
           >
