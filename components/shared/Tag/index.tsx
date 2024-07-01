f2f373a81f650fa80cf310be13ee94cc5d9e1e81
thisisthewa2 <thisisthewa2@gmail.com>
2024-07-01 21:53:39 +0900

    [Style] 태그 선택되었을 경우 border 추가

--- components/shared/Tag/index.tsx
+++ components/shared/Tag/index.tsx
@@ -10,7 +10,8 @@ function Tag({ children, tagType, tagId, selected, onClick }: TagProps) {
   } else {
     switch (tagType) {
       case 'blue':
-        tagStyle = 'bg-suldak-mint-50 text-suldak-mint-500 text-[14px] font-medium';
+        tagStyle =
+          'bg-suldak-mint-50 border border-suldak-mint-500 text-suldak-mint-500 text-[14px] font-medium';
         break;
       case 'gray':
         tagStyle =
