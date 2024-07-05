243051bd665c5be25811bfc2e33a3d05a9c6fc95
thisisthewa2 <thisisthewa2@gmail.com>
2024-07-05 16:15:20 +0900

    [Fix] Interceptor/Parallel Router을 사용한 팝업 페이지 분리

--- components/liquor/search/FilterButton.tsx
+++ components/liquor/search/FilterButton.tsx
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
 import FilterIcon from 'assets/icons/ico-filter-filter.svg';
-import FilterPopup from './FilterPopup';
-function FilterButton() {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const openPopup = () => setIsPopupOpen(true);
-  const closePopup = () => setIsPopupOpen(false);
-
+interface FilterButtonProps {
+  onClick: () => void;
+}
+function FilterButton({ onClick }: FilterButtonProps) {
   return (
     <>
       <button
         className="flex text-suldak-gray-600 text-[14px] items-center gap-0.5"
-        onClick={openPopup}
+        onClick={onClick}
       >
         <FilterIcon />
         필터
       </button>
-
-      <FilterPopup isOpen={isPopupOpen} onClose={closePopup} />
     </>
   );
 }
