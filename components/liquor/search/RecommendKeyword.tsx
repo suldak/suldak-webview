24913e56c4d977adaf2dcc1603f00d6a559efd2f
thisisthewa2 <thisisthewa2@gmail.com>
2024-08-06 15:59:49 +0900

    [Feat] 추천 검색어 태그 라우팅 연결

--- components/liquor/search/RecommendKeyword.tsx
+++ components/liquor/search/RecommendKeyword.tsx
@@ -1,17 +1,27 @@
 'use client';
 
 import { useGetRecommendKeyword } from 'apis/keyword/useGetRecommendKeyword';
+import { useLiquorSearch } from 'apis/liquor/useLiquorSearch';
 
 import Tag from 'components/shared/Tag';
+import { useRouter } from 'next/navigation';
 
 /** 추천 검색어 컴포넌트 */
 function RecommendKeyword() {
+  const router = useRouter();
   const { data: recommendKeywords } = useGetRecommendKeyword();
-
+  const handleClick = (id: number) => {
+    router.push(`/liquor/search/result/${id}`);
+  };
   return (
     <>
       {recommendKeywords.map((recLiquor) => (
-        <Tag tagId={recLiquor.id} key={recLiquor.id} tagType="blue">
+        <Tag
+          tagId={recLiquor.id}
+          key={recLiquor.id}
+          tagColor="blue"
+          onClick={handleClick}
+        >
           {recLiquor.text}
         </Tag>
       ))}
--- components/shared/Tag/types.d.ts
+++ components/shared/Tag/types.d.ts
@@ -5,14 +5,12 @@ type TagColor = 'gray' | 'blue';
 /**
  * 태그 인터페이스
  */
-interface TagProps extends ComponentPropsWithoutRef<'div'> {
+interface TagProps extends Omit<ComponentPropsWithoutRef<'span'>, 'onClick'> {
   tagId: number;
   onClick?: (tagId: number) => void;
   tagColor?: TagColor;
   selected?: boolean;
-  children?: React.ReactNode;
 }
-
 /**
  * 클릭 이벤트가 있는 태그 인터페이스
  */
