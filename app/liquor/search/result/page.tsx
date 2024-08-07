2a7ba3843ff7f8a681b130d36212f6b5d4a4c659
thisisthewa2 <thisisthewa2@gmail.com>
2024-08-07 19:03:13 +0900

    Feat: 검색결과가 없는 경우 페이지 UI 구현

--- app/liquor/search/result/page.tsx
+++ app/liquor/search/result/page.tsx
@@ -9,6 +9,7 @@ import SortDropDown from 'components/liquor/search/SortDropDown';
 import LiquorCard from 'components/shared/LiquorCard';
 import FilterButton from 'components/liquor/search/FilterButton';
 import { useEffect, useState } from 'react';
+import NoResultSection from 'components/liquor/search/section/NoResultSection';
 
 function LiquorSearchResultPage() {
   const searchParams = useSearchParams();
@@ -18,7 +19,6 @@ function LiquorSearchResultPage() {
     // 검색 파라미터가 변경될 때마다 새로운 검색 키 생성
     setSearchKey(searchParams.toString());
   }, [searchParams]);
-
 
   const { data, isLoading, error } = useLiquorSearch(
     {
@@ -32,59 +32,69 @@ function LiquorSearchResultPage() {
     searchKey,
   );
   const liquors: Liquor[] = data?.data.content || [];
-
-  return (
-    <main>
-      <SearchInput />
-      {/* 추천 목록 */}
-      <section className="border-b border-suldak-gray-200">
-        <div className="flex items-center gap-2 px-5 py-3.5">
-          <span className="text-suldak-gray-900 text-sm font-semibold">
-            추천
-          </span>
-          <div className="text-suldak-gray-500">|</div>
-          <div className="flex items-center gap-4 text-suldak-mint-500 text-sm font-semibold">
-            <span>직장인</span>
-            <span>위스키 베이스</span>
-            <span>칵테일</span>
-          </div>
+  if (!liquors.length) {
+    return (
+      <main className="flex flex-col min-h-screen">
+        <SearchInput />
+        <div className="flex-grow flex items-center justify-center">
+          <NoResultSection />
         </div>
-      </section>
-
-      {/* 술 검색 목록 */}
-      <section className="px-5">
-        <div className="flex items-center justify-between pt=3.5">
-          <span className="text-xs font-medium text-suldak-gray-600">
-            총 {liquors.length ?? 0}종
-          </span>
-
-          <div className="flex items-center gap-3 text-sm text-suldak-gray-600 font-medium leading-5">
-            <SortDropDown />
-            <FilterButton />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <SearchInput />
+        {/* 추천 목록 */}
+        <section className="border-b border-suldak-gray-200">
+          <div className="flex items-center gap-2 px-5 py-3.5">
+            <span className="text-suldak-gray-900 text-sm font-semibold">
+              추천
+            </span>
+            <div className="text-suldak-gray-500">|</div>
+            <div className="flex items-center gap-4 text-suldak-mint-500 text-sm font-semibold">
+              <span>직장인</span>
+              <span>위스키 베이스</span>
+              <span>칵테일</span>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section
-        className="flex flex-col px-5 py-3.5 gap-2.5 overflow-y-auto"
-        style={{ maxHeight: `calc(100dvh - 100px)` }}
-      >
-        {liquors.map((liquor: Liquor) => (
-          <LiquorCard
-            key={liquor.id} // 고유한 key prop 추가
-            imgUrl={liquor.liquorPictureUrl || '/default-image-url.jpg'} // 유효하지 않은 URL 처리
-            liquorId={liquor.id}
-            liquorDetail={liquor.detailExplanation}
-            liquorAbv={liquor.detailAbv}
-            name={liquor.name}
-            liquorSellDtos={liquor.liquorSellDtos}
-            liquorSnackRes={liquor.liquorSnackRes}
-            tasteTypeDtos={liquor.tasteTypeDtos}
-          />
-        ))}
-      </section>
-    </main>
-  );
+        {/* 술 검색 목록 */}
+        <section className="px-5">
+          <div className="flex items-center justify-between pt=3.5">
+            <span className="text-xs font-medium text-suldak-gray-600">
+              총 {liquors.length ?? 0}종
+            </span>
+
+            <div className="flex items-center gap-3 text-sm text-suldak-gray-600 font-medium leading-5">
+              <SortDropDown />
+              <FilterButton />
+            </div>
+          </div>
+        </section>
+
+        <section
+          className="flex flex-col px-5 py-3.5 gap-2.5 overflow-y-auto"
+          style={{ maxHeight: `calc(100dvh - 100px)` }}
+        >
+          {liquors.map((liquor: Liquor) => (
+            <LiquorCard
+              key={liquor.id} // 고유한 key prop 추가
+              imgUrl={liquor.liquorPictureUrl || '/default-image-url.jpg'} // 유효하지 않은 URL 처리
+              liquorId={liquor.id}
+              liquorDetail={liquor.detailExplanation}
+              liquorAbv={liquor.detailAbv}
+              name={liquor.name}
+              liquorSellDtos={liquor.liquorSellDtos}
+              liquorSnackRes={liquor.liquorSnackRes}
+              tasteTypeDtos={liquor.tasteTypeDtos}
+            />
+          ))}
+        </section>
+      </main>
+    );
+  }
 }
 
 export default LiquorSearchResultPage;
