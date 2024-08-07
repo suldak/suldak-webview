d0e73a5f2cfd151579b50817d31b4457787d9868
thisisthewa2 <thisisthewa2@gmail.com>
2024-08-07 20:31:07 +0900

    Feat: 추천검색어 스켈레톤 UI 적용

--- components/liquor/search/section/RecommendSearchSection.tsx
+++ components/liquor/search/section/RecommendSearchSection.tsx
@@ -1,18 +1,27 @@
 'use client';
 import { Suspense } from 'react';
 import RecommendKeyword from '../RecommendKeyword';
+import SkeletonTag from 'components/shared/Tag/SkeletonTag';
 
 function RecommendedSearchSection() {
   return (
     <section className="px-5">
       <p className="text-base font-bold pt-10 pb-2">추천 검색어</p>
       <div className="flex flex-wrap gap-2 py-2">
-        <Suspense fallback={<>로딩중...</>}>
+        <Suspense fallback={<SkeletonLoading />}>
           <RecommendKeyword />
         </Suspense>
       </div>
     </section>
   );
 }
-
+function SkeletonLoading() {
+  return (
+    <>
+      {[...Array(6)].map((_, index) => (
+        <SkeletonTag key={index} />
+      ))}
+    </>
+  );
+}
 export default RecommendedSearchSection;
