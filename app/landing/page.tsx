2e85845eed4a039decd2aad8f08c9206f280b42b
thisisthewa2 <thisisthewa2@gmail.com>
2024-08-21 15:28:27 +0900

    Refactor: toast 컴포넌트 분리

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
-import { Toaster } from 'react-hot-toast';
 import CopySection from 'components/landing/CopySection';
 import ServiceSection from 'components/landing/ServiceSection';
 import ReviewSection from 'components/landing/ReviewSection';
@@ -8,6 +7,7 @@ import Header from 'components/landing/Header';
 import GroupSection from 'components/landing/GroupSection';
 import ReservationSection from 'components/landing/ReservationSection';
 import NavigationBar from 'components/landing/NavigationBar';
+import ReservationToaster from 'components/landing/ReservationToaster';
 
 function LandingPage() {
   const [isMounted, setIsMounted] = useState(false);
@@ -34,41 +34,7 @@ function LandingPage() {
       <ReviewSection />
       <ReservationSection ref={reservationRef} />
       <NavigationBar scrollToReservation={scrollToReservation} />
-      <div className="flex w-full text-black text-[25px] shadow-suldak-card">
-        <Toaster
-          position="top-center"
-          containerStyle={{
-            top: 300,
-          }}
-          toastOptions={{
-            duration: 3000,
-            style: {
-              padding: '16px 24px',
-              maxWidth: '90vw',
-              width: 'auto',
-              fontSize: '25px',
-              lineHeight: '1.5',
-              display: 'flex',
-              alignItems: 'center',
-              gap: '12px',
-              background: 'white',
-              color: 'black',
-              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-              borderRadius: '8px',
-            },
-            success: {
-              icon: (
-                <span style={{ fontSize: '30px', marginRight: '8px' }}>✅</span>
-              ),
-            },
-            error: {
-              icon: (
-                <span style={{ fontSize: '30px', marginRight: '8px' }}>❌</span>
-              ),
-            },
-          }}
-        />
-      </div>
+      <ReservationToaster />
     </div>
   );
 }
