9cd59f9870fb991c34ba6b996e55b127717ead2c
thisisthewa2 <thisisthewa2@gmail.com>
2024-08-22 17:39:17 +0900

    Style: CopySection 모바일 스타일링

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import HeadRight from 'assets/icons/ico-head-right.svg';
-import HeadImg from 'assets/pngs/image-head.png';
-import HeadSmallImg from 'assets/pngs/image-head-small.png';
+import Image from "next/image";
+import HeadRight from "assets/icons/ico-head-right.svg";
+import HeadImg from "assets/pngs/image-head.png";
+import HeadSmallImg from "assets/pngs/image-head-small.png";
 
 interface HeaderProps {
   scrollToReservation: () => void;
@@ -10,7 +10,7 @@ interface HeaderProps {
 function Header({ scrollToReservation }: HeaderProps) {
   return (
     <>
-      <div className="w-full relative pc:pb-[46%]">
+      <div className="relative w-full pc:h-[900px]">
         <Image
           src={HeadImg}
           alt="Header Image"
@@ -18,7 +18,7 @@ function Header({ scrollToReservation }: HeaderProps) {
           objectFit="cover"
         />
       </div>
-      <div className="pc:invisible mobile:w-full relative mobile:h-[600px] pc:h-0">
+      <div className="relative mobile:h-[600px] mobile:w-full pc:hidden">
         <Image
           src={HeadSmallImg}
           alt="Header Image"
@@ -26,20 +26,20 @@ function Header({ scrollToReservation }: HeaderProps) {
           objectFit="cover"
         />
       </div>
-      <section className="absolute top-[100px] left-1/2 transform -translate-x-1/2 text-center">
-        <div className="mb-4 text-[42px] mobile:text-[20px] font-medium font-GMarket text-white text-nowrap">
+      <section className="absolute left-1/2 top-[100px] -translate-x-1/2 transform text-center">
+        <div className="mb-4 text-nowrap font-GMarket text-[42px] font-medium text-white mobile:text-[20px]">
           나와 우리, 그리고 한 잔의 술
           <div className="text-[64px] font-bold mobile:text-[36px]">
-            술닥술닥에서 <br className="pc:invisible" />한 잔 하실래요?
+            술닥술닥에서 <br className="pc:hidden" />한 잔 하실래요?
           </div>
         </div>
-        <div className="text-[20px] mobile:text-[14px] text-white my-[16px] font-medium">
+        <div className="my-[16px] text-[20px] font-medium text-white mobile:text-[14px]">
           관심사를 함께 나누며 즐겁게 한 잔!
           <br />
           당신의 취향을 술닥술닥에서 만나보세요.
         </div>
         <button
-          className="bg-white text-suldak-mint-500 flex font-bold items-center px-[20px] py-[16px] rounded-full mx-auto"
+          className="mx-auto flex items-center rounded-full bg-white px-[20px] py-[16px] font-bold text-suldak-mint-500"
           onClick={scrollToReservation}
         >
           지금 사전예약하기
