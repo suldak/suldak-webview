4607bdb323ff58db423c2c5f089687093164831e
thisisthewa2 <thisisthewa2@gmail.com>
2024-10-07 20:48:23 +0900

    Fix: useGetLiquorDetail api 경로 변경

--- apis/liquor/useGetLiquorDetail.ts
+++ apis/liquor/useGetLiquorDetail.ts
@@ -1,24 +1,24 @@
-import axiosInstance from 'apis/axiosInstance';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { ResponseType } from 'apis/api';
-import { Liquor } from 'models/liquor';
+import axiosInstance from "apis/axiosInstance";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ResponseType } from "apis/api";
+import { Liquor } from "models/liquor";
 
 const getLiquorDetail = async (id: number): Promise<ResponseType<Liquor>> => {
   try {
     const { data } = await axiosInstance.get<ResponseType<Liquor>>(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/liquor/${id}`,
+      `http://122.45.170.200:8080/api/liquor/${id}`,
     );
-   
+
     return data;
   } catch (error) {
-    console.error('Error fetching liquor detail:', error);
+    console.error("Error fetching liquor detail:", error);
     throw error;
   }
 };
 
 export const useGetLiquorDetail = (id: number) => {
   const { data } = useSuspenseQuery({
-    queryKey: ['liquor-detail', id],
+    queryKey: ["liquor-detail", id],
     queryFn: () => getLiquorDetail(id),
   });
 
