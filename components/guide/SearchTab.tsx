f46d3b2a359ec3cacf8c1b683abf281765270829
thisisthewa2 <thisisthewa2@gmail.com>
2024-11-11 23:23:10 +0900

    Style: SearchTab정렬 변경

--- components/guide/SearchTab.tsx
+++ components/guide/SearchTab.tsx
@@ -1,20 +1,21 @@
-import SearchTabImg from 'assets/icons/ico-search-tab.svg';
-import ResultTabImg from 'assets/icons/ico-result-tab.svg';
-import FilterTabImg from 'assets/icons/ico-filter-tab.svg';
+import SearchTabImg from "assets/icons/ico-search-tab.svg";
+import ResultTabImg from "assets/icons/ico-result-tab.svg";
+import FilterTabImg from "assets/icons/ico-filter-tab.svg";
 function SearchTab() {
   return (
-    <>
-      <p className="mb-[20px]">
+    <div className="w-full flex-col items-center justify-center">
+      <p className="mb-[20px] px-[20px]">
         [홈] 탭의 [검색바]를 통해 여러 술 제품을 찾아보고,
-        <br /> 나에게 맞는 술을 찾을 수 있어요
+        나에게 맞는 술을 찾을 수 있어요
       </p>
-
-      <SearchTabImg />
-      <div className="mb-[40px]">
-        <h3 className="mt-[12px] text-[16px] text-suldak-mint-500 font-semibold">
+      <div className="flex w-full items-center justify-center">
+        <SearchTabImg />
+      </div>
+      <div className="mb-[40px] px-[20px]">
+        <h3 className="mt-[12px] text-[16px] font-semibold text-suldak-mint-500">
           추천 키워드, 랭킹
         </h3>
-        <ul className="space-y-2 list-disc pl-[20px]">
+        <ul className="list-disc space-y-2 pl-[20px]">
           <li className="indent">
             [추천 검색어]로 빠르고 간단하게 지금 마시고 싶은 술을 검색해봐요!
           </li>
@@ -24,20 +25,22 @@ function SearchTab() {
           </li>
         </ul>
       </div>
-      <FilterTabImg />
-      <div className="mt-[12px] mb-[40px]">
-        <h3 className=" text-[16px] text-suldak-mint-500 font-semibold">
-          필터
-        </h3>
-        <ul className="space-y-2 list-disc pl-[20px]">
+      <div className="flex w-full items-center justify-center">
+        <FilterTabImg />
+      </div>
+      <div className="mb-[40px] mt-[12px] px-[20px]">
+        <h3 className="text-[16px] font-semibold text-suldak-mint-500">필터</h3>
+        <ul className="list-disc space-y-2 pl-[20px]">
           <li className="indent">
             자세한 검색이 필요하다면 [필터]를 이용해 술을 검색해 보세요
           </li>
         </ul>
       </div>
-      <ResultTabImg />
-      <div className="mt-[12px] mb-[20px]">
-        <h3 className="text-[16px] mt-[12px] text-suldak-mint-500 font-semibold">
+      <div className="flex w-full items-center justify-center">
+        <ResultTabImg />
+      </div>
+      <div className="mb-[20px] mt-[12px] px-[20px]">
+        <h3 className="mt-[12px] text-[16px] font-semibold text-suldak-mint-500">
           결과, 제품페이지
         </h3>
         <li>
@@ -49,7 +52,7 @@ function SearchTab() {
           <p className="ml-[18px]">포함하고 있어 직접 만들어 볼 수도 있어요!</p>
         </li>
       </div>
-    </>
+    </div>
   );
 }
 
