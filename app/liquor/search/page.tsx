6b57c7f92ee509d1860d4f255bf874d793121096
thisisthewa2 <thisisthewa2@gmail.com>
2024-11-29 19:18:13 +0900

    Refactor:중복되는 skeleton코드 삭제

--- app/liquor/search/page.tsx
+++ app/liquor/search/page.tsx
@@ -4,18 +4,16 @@ import { Suspense } from "react";
 import RecentSearchSection from "components/liquor/search/section/RecentSearchSection";
 import RecommendedSearchSection from "components/liquor/search/section/RecommendSearchSection";
 import SearchRankingSection from "components/liquor/search/section/SearchRankingSection";
-import RecommendSearchSkeleton from "components/liquor/skeleton/RecommendSearchSection";
-import SearchRankingSkeleton from "components/liquor/skeleton/SearchRankingSkeleton";
 
 /** 술 검색 페이지 */
 function LiquorSearchPage() {
   return (
     <>
       <RecentSearchSection />
-      <Suspense fallback={<RecommendSearchSkeleton />}>
+      <Suspense>
         <RecommendedSearchSection />
       </Suspense>
-      <Suspense fallback={<SearchRankingSkeleton />}>
+      <Suspense>
         <SearchRankingSection />
       </Suspense>
     </>
