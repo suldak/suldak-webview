09bc82a5d631b5041df861c0c80bcc2e431c97ed
thisisthewa2 <thisisthewa2@gmail.com>
2025-03-09 22:10:39 +0900

    Style: LiquorCard사이 간격수정

--- components/liquor/category/LiquorList.tsx
+++ components/liquor/category/LiquorList.tsx
@@ -1,20 +1,22 @@
 import LiquorCard from "components/shared/LiquorCard";
 import { Liquor } from "models/liquor";
+
 function LiquorList({ liquors }: { liquors: Liquor[] }) {
   return (
-    <section className="gap-y-2. flex h-full w-full flex-col items-center justify-center">
+    <section className="flex h-full w-full flex-col items-center justify-center">
       {liquors.map((liquor: Liquor) => (
-        <LiquorCard
-          key={liquor.id}
-          imgUrl={liquor.liquorPictureUrl}
-          liquorId={liquor.id}
-          liquorDetail={liquor.summaryExplanation}
-          liquorAbv={liquor.detailAbv}
-          name={liquor.name}
-          liquorSellDtos={liquor.liquorSellDtos}
-          liquorSnackRes={liquor.liquorSnackRes}
-          tasteTypeDtos={liquor.tasteTypeDtos}
-        />
+        <div key={liquor.id} className="mt-[10px]">
+          <LiquorCard
+            imgUrl={liquor.liquorPictureUrl}
+            liquorId={liquor.id}
+            liquorDetail={liquor.summaryExplanation}
+            liquorAbv={liquor.detailAbv}
+            name={liquor.name}
+            liquorSellDtos={liquor.liquorSellDtos}
+            liquorSnackRes={liquor.liquorSnackRes}
+            tasteTypeDtos={liquor.tasteTypeDtos}
+          />
+        </div>
       ))}
     </section>
   );
