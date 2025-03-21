dfa8b380a94fbf6b926cb0bea22c4225f48e0ba9
thisisthewa2 <thisisthewa2@gmail.com>
2025-03-21 23:59:15 +0900

    Feat: 페이지에서 토큰을 요청하는 코드 추가

--- app/liquor/search/page.tsx
+++ app/liquor/search/page.tsx
@@ -4,9 +4,12 @@ import { Suspense } from "react";
 import RecentSearchSection from "components/liquor/search/section/RecentSearchSection";
 import RecommendedSearchSection from "components/liquor/search/section/RecommendSearchSection";
 import SearchRankingSection from "components/liquor/search/section/SearchRankingSection";
+import { useFlutterToken } from "../hooks/useFlutterToken";
 
 /** 술 검색 페이지 */
 function LiquorSearchPage() {
+  // 이 페이지에서만 플러터 토큰을 요청
+  useFlutterToken();
   return (
     <>
       <RecentSearchSection />
