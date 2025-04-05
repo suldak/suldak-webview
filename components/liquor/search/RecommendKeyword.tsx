2828285b861f29b8d27c861efdfc8c98e0b30636
thisisthewa2 <thisisthewa2@gmail.com>
2025-04-05 19:54:49 +0900

    Fix: 데이터가 없을 경우 대비해 RankingKeyword, RecommendKeyword 초기값을 빈배열로 변경

--- components/liquor/search/RecommendKeyword.tsx
+++ components/liquor/search/RecommendKeyword.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 /** 추천 검색어 컴포넌트 */
 function RecommendKeyword() {
   const router = useRouter();
-  const { data: recommendKeywords } = useGetRecommendKeyword();
+  const { data: recommendKeywords = [] } = useGetRecommendKeyword();
 
   const handleClick = (
     event: React.MouseEvent<HTMLSpanElement>,
@@ -19,7 +19,7 @@ function RecommendKeyword() {
 
   return (
     <>
-      {recommendKeywords.map((recLiquor) => (
+      {recommendKeywords?.map((recLiquor) => (
         <Tag
           tagId={recLiquor.id}
           key={recLiquor.id}
--- components/liquor/search/ranking/RankingKeyword.tsx
+++ components/liquor/search/ranking/RankingKeyword.tsx
@@ -1,20 +1,25 @@
 "use client";
+
 import { useGetRankingKeyword } from "apis/keyword/useGetRankingKeyword";
 import RankingKeywordList from "./RankingKeywordList";
 
 function RankingKeyword() {
-  const { data: rankingKeywords } = useGetRankingKeyword();
+  const { data: rankingKeywords = [] } = useGetRankingKeyword();
 
   return (
     <div className="flex items-center gap-14">
-      <RankingKeywordList
-        keywords={rankingKeywords.slice(0, 5)}
-        startIndex={0}
-      />
-      <RankingKeywordList
-        keywords={rankingKeywords.slice(5, 10)}
-        startIndex={5}
-      />
+      {rankingKeywords.length > 0 && (
+        <>
+          <RankingKeywordList
+            keywords={rankingKeywords.slice(0, 5)}
+            startIndex={0}
+          />
+          <RankingKeywordList
+            keywords={rankingKeywords.slice(5, 10)}
+            startIndex={5}
+          />
+        </>
+      )}
     </div>
   );
 }
