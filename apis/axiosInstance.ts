3e17db20411b01e7ca27d0eafae04f2650bb2e8f
thisisthewa2 <thisisthewa2@gmail.com>
2025-04-10 21:28:26 +0900

    Fix: 토큰 스타일 일치

--- apis/axiosInstance.ts
+++ apis/axiosInstance.ts
@@ -10,17 +10,17 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use((config) => {
   const token = getToken();
 
-  if (token) {
-    // 플러터 앱에서 요구하는 형식으로 설정 ("authorization = token")
-    config.headers["authorization"] = `token ${token}`;
-  } else {
-    // 토큰이 없으면 환경변수 사용
-    const envToken = process.env.NEXT_PUBLIC_TOKEN;
-    if (envToken) {
-      config.headers["authorization"] = envToken;
-      console.log("환경변수 토큰을 사용중입니다.") //테스트용 코드
-    }
-  }
+ if (token) {
+   // 플러터 앱과 같은 형식으로 설정
+   config.headers["Authorization"] = token;
+ } else {
+   // 토큰이 없으면 환경변수 사용 
+   const envToken = process.env.NEXT_PUBLIC_TOKEN;
+   if (envToken) {
+     config.headers["Authorization"] = envToken;
+     console.log("환경변수 토큰을 사용중입니다.");
+   }
+ }
 
   return config;
 });
