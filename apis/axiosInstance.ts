f0ff1c95255614b6e38b97d2e12e965da9aabef9
thisisthewa2 <thisisthewa2@gmail.com>
2025-04-10 21:20:09 +0900

    Style:토큰 오타 수정

--- apis/axiosInstance.ts
+++ apis/axiosInstance.ts
@@ -11,12 +11,12 @@ axiosInstance.interceptors.request.use((config) => {
 
   if (token) {
     // 플러터 앱에서 요구하는 형식으로 설정 ("authorization = token")
-    config.headers["Authorization"] = `token ${token}`;
+    config.headers["authorization"] = `token ${token}`;
   } else {
     // 토큰이 없으면 환경변수 사용 
     const envToken = process.env.NEXT_PUBLIC_TOKEN;
     if (envToken) {
-      config.headers["Authorization"] = envToken;
+      config.headers["authorization"] = envToken;
       console.log("환경변수 토큰을 사용중입니다.") //테스트용 코드
     }
   }
