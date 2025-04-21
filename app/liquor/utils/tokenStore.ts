081ec586734dd1799a67c3ac1f4be53073d917a3
thisisthewa2 <thisisthewa2@gmail.com>
2025-04-21 16:42:34 +0900

    Fix: 로컬스토리지에 토큰을 저장하도록 변경

--- app/liquor/utils/tokenStore.ts
+++ app/liquor/utils/tokenStore.ts
@@ -5,10 +5,15 @@ let userToken: string | null = null;
 export const setToken = (token: string) => {
   if (typeof window === "undefined") return null;
   userToken = token;
+  localStorage.setItem("authToken", token); //로컬스토리지를 사용해 토큰 저장
   //console.log("Token set:", token);
 };
 
 export const getToken = () => {
   if (typeof window === "undefined") return;
+  if (!userToken && typeof window !== "undefined") {
+    userToken = localStorage.getItem("authToken");
+  }
+
   return userToken;
 };
