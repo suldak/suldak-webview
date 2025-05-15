d481ffc853e8b81fe84ce33c89353fe767da80d4
thisisthewa2 <thisisthewa2@gmail.com>
2025-05-15 22:11:34 +0900

    Style: 필터 버튼을 고정 픽셀이 아닌 비율로 변경

--- components/liquor/search/FilterResetButton.tsx
+++ components/liquor/search/FilterResetButton.tsx
@@ -6,7 +6,7 @@ interface FilterResetButtonProps {
 function FilterResetButton({ onReset }: FilterResetButtonProps) {
   return (
     <button
-      className="flex h-[60px] w-[81px] items-center justify-center gap-x-[10px] rounded-[15px] py-[20px] text-[18px] font-medium text-suldak-gray-500 xs:text-[15px]"
+      className="flex h-[60px] w-full items-center justify-center gap-x-[10px] rounded-[15px] py-[20px] text-[18px] font-medium text-suldak-gray-500 xs:text-[15px]"
       onClick={onReset}
     >
       <ReturnIcon />
