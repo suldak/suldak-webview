e8055ef70df2d045fb7fdc9d6b2e9f15498265b6
thisisthewa2 <thisisthewa2@gmail.com>
2025-11-26 01:09:46 +0900

    refactor: Inter 폰트 제거 및 preconnect/preload 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -10,10 +7,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`mx-auto bg-white font-pretendard text-suldak-gray-900 ${inter.className}`}
-      >
+    <html lang="ko">
+      <head>
+        {/* 외부 출처 사전 연결로 초기 연결 시간 절약 */}
+        <link rel="preconnect" href="https://fastly.jsdelivr.net" />
+        <link rel="dns-prefetch" href="https://fastly.jsdelivr.net" />
+        <link rel="preconnect" href="https://api.suldak.co.kr" />
+        <link rel="dns-prefetch" href="https://api.suldak.co.kr" />
+
+        {/* 폰트 preload로 렌더링 차단 최소화 */}
+        <link
+          rel="preload"
+          href="https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff"
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff"
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className="mx-auto bg-white font-pretendard text-suldak-gray-900">
         <Provider>{children}</Provider>
       </body>
     </html>
